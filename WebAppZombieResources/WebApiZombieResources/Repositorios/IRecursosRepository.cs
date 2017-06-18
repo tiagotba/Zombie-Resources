20e14196266c18e364ddf36c28904204c58ce602
unknown <osbentoalves@gmail.com>
2017-06-18 13:16:44 -0300

    criacao dos repositorios

---
+++ WebAppZombieResources/WebApiZombieResources/Repositorios/IRecursosRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiZombieResources.Models;
+
+namespace WebApiZombieResources.Repositorios
+{
+    public interface IRecursosRepository
+    {
+        List<Recursos> GetRecursosList();
+        void CreateRecursos(Recursos modelo);
+        void EditaMovimentacoesRecursos(string id);
+        void EditaDetalhesRecursos(string id);
+
+    }
+}
---
+++ WebAppZombieResources/WebApiZombieResources/Repositorios/ISobreviventeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiZombieResources.Models;
+
+namespace WebApiZombieResources.Repositorios
+{
+   public interface ISobreviventeRepository
+    {
+        List<Sobrevivente> GetSobreviventesList();
+        void CreateSobreviventes(Sobrevivente modelo);
+    }
+}
