840eabf2f5157a8a6a0e6def9feba4df460cf7a3
unknown <osbentoalves@gmail.com>
2017-06-18 10:40:40 -0300

    Criacao das classes de modelo Poco

---
+++ WebAppZombieResources/WebApiZombieResources/Models/Recursos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiZombieResources.Models
+{
+    public class Recursos
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public string Observacao { get; set; }
+        public virtual Sobrevivente Sobrevivente { get; set; }
+    }
+}
---
+++ WebAppZombieResources/WebApiZombieResources/Models/Sobrevivente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiZombieResources.Models
+{
+    public class Sobrevivente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+        public int Idade { get; set; }
+        public virtual List<Recursos> Recursos { get; set; }
+    }
+}
