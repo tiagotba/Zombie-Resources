7d5d11d848f59f97b1c6e774d8ac26f8fe817428
unknown <osbentoalves@gmail.com>
2017-06-18 11:29:32 -0300

    criacao da classe de contexto e suas dependencias

---
+++ WebAppZombieResources/WebApiZombieResources/AcessoDados/ZombieResourcesDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApiZombieResources.Models;
+
+namespace WebApiZombieResources.AcessoDados
+{
+    public class ZombieResourcesDbContext : DbContext
+    {
+        public ZombieResourcesDbContext()
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        public DbSet<Recursos> Recursos { get; set; }
+        public DbSet<Sobrevivente> Sobreviventes { get; set; }
+    }
+}
